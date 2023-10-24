21cc18537c291cdf005f904d626235fb5c275477
Jeniffer souza <ajenifferbsouza@gmail.com>
2023-10-23 21:41:37 -0300

    add
    add

---
+++ src/model/Usuario.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.sql.Date;
+
+final class Usuario   extends Pessoa{
+	
+	private int id;
+	private String nome;
+	private String sobrenome;
+	private String senha;
+	private Date data_cadastro;
+	private boolean habalitado;
+
+
+}
---
+++ src/model/Vendedor.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.sql.Date;
+
+public class Vendedor extends Pessoa {
+
+
+	private int id;
+	private String nome;
+	private String sobrenome;
+	private String senha;
+	private Date data_entrada;
+	private boolean habalitado;
+}
