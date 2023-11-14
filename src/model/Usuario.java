a0ce1ec7c019175678f46c55bf739f2b02ef5e6a
Jeniffer souza <ajenifferbsouza@gmail.com>
2023-11-14 19:15:33 -0300

    Update Usuario.java

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,51 +1,56 @@
 package model;
 
-<<<<<<< HEAD
-import java.util.Date;
+import java.sql.Date;
+
 
 public final class Usuario extends Pessoa{
 	
-	private String senha;
-	private String data_cadastro;
-	private boolean habilitado;
-
-	
-	public String getData_cadastro() {
-		
-		return data_cadastro;
-	}
-	public void setData_cadastro(String data_cadastro) {
-		this.data_cadastro = data_cadastro;
-	}
-	
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
-	public boolean isHabilitado() {
-		return habilitado;
-	}
-	public void setHabilitado(boolean habilitado) {
-		this.habilitado = habilitado;
-	}
-	
-	
-=======
-import java.sql.Date;
-
-final class Usuario   extends Pessoa{
 	
 	private int id;
 	private String nome;
 	private String sobrenome;
 	private String senha;
 	private Date data_cadastro;
-	private boolean habalitado;
+	private boolean habilitado;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Date getData_cadastro() {
+		return data_cadastro;
+	}
+	public void setData_cadastro(Date data_cadastro) {
+		this.data_cadastro = data_cadastro;
+	}
+	public boolean isHabilitado() {
+		return habilitado;
+	}
+	public void setHabilitado(boolean habilitado) {
+		this.habilitado = habilitado;
+	}
 
-
->>>>>>> 2e87b6bbd387d8ba09749d839e57e3174e815ec4
+	
+	
+	
 }
